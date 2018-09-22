5c72a33c9ddaba34bd9b569ec46e21f4c09ab45e
Olumide Okedusi <odokedusi@sidmach.com>
2018-09-22 06:09:08 +0100

    Implementation of validation on fields

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 const User = require("../../models/User");
 
 // @route   GET api/users/test
@@ -17,11 +20,17 @@ router.get("/test", (req, res) => res.json({ msg: "Inside the users route" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({
-        email: "Email already exists"
-      });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
