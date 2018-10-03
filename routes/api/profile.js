482eb2262c33b0f6e0ed6c25a758c4d066de95c7
Olumide Okedusi <odokedusi@sidmach.com>
2018-10-03 09:58:34 +0100

    Update routes directory

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,7 +5,8 @@ const mongoose = require("mongoose");
 
 //Load profile validation
 const validateProfileInput = require("../../validation/profile");
-
+const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 //Load profile model
 const Profile = require("../../models/Profile");
 //Load user model
@@ -41,6 +42,24 @@ router.get(
   }
 );
 
+// @route   GET api/profile/all
+// @desc    Get all profile
+// @access  Public
+router.get("/all", (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles here";
+        return res.status(404).json(errors);
+      } else {
+        return res.json(profiles);
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route   GET api/profile/handle/:handle
 // @desc    Get profile by handle
 // @access  Public
@@ -74,7 +93,9 @@ router.get("/user/:user_id", (req, res) => {
         res.json(profile);
       }
     })
-    .catch(err => res.status(404).json(err));
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
 });
 
 // @route   POST api/profile
@@ -140,6 +161,68 @@ router.post(
           }
         });
       }
+    });
+  }
+);
+
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      //Return errors with status 400
+      return res.status(400).json(errors);
+    }
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        location: req.body.location,
+        description: req.body.description
+      };
+      //Add to experience array
+      profile.experience.unshift(newExp);
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      //Return errors with status 400
+      return res.status(400).json(errors);
+    }
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        fieldofstudy: req.body.fieldofstudy,
+        description: req.body.description
+      };
+      //Add to experience array
+      profile.education.unshift(newEdu);
+      profile.save().then(profile => res.json(profile));
     });
   }
 );
