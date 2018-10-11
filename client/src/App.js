194d07143d4e1b87cd293ff73bc39383e4d5c854
Olumide Okedusi <odokedusi@sidmach.com>
2018-10-11 06:39:29 +0100

    updated index.html, app.css and app.js files to achieve desired output

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Landing from "./components/layouts/Landing";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Olumide says hello to CRA 2.0</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
