f563fc5bba3294cac3130272faece562716a25ab
Olumide <olumideokedusi@gmail.com>
2018-11-06 16:07:09 +0100

    Import created group types into create-profile component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
+import SelectListGroup from "../common/SelectListGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import InputGroup from "../common/InputGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
