20c612c8ff0ccbb6255d1cf933bc7433ebdb8e2d
Olumide <olumideokedusi@gmail.com>
2018-11-11 23:39:09 +0100

    Create directory to handle adding experience and education

---
+++ client/src/components/add-credentials/AddExperience.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { TextFieldGroup } from "../common/TextFieldGroup";
+import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
+import PropTypes from "prop-types";
+
+class AddExperience extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      company: "",
+      title: "",
+      location: "",
+      from: "",
+      to: "",
+      current: false,
+      description: "",
+      errors: {},
+      disabled: false
+    };
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+AddExperience.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  return {
+    profile: state.profile,
+    errors: state.errors
+  };
+};
+
+export default connect(mapStateToProps)(withRouter(AddExperience));
