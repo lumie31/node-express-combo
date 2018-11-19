44d3edaa40c1261c97cc11de2c89b015b79cc80a
Olumide <olumideokedusi@gmail.com>
2018-11-20 00:06:40 +0100

    Added go-back button to edit profile page

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
 import SelectListGroup from "../common/SelectListGroup";
@@ -190,6 +190,13 @@ class CreateProfile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-10 m-auto">
+              <Link
+                style={{ marginTop: "10vh" }}
+                to="/dashboard"
+                className="btn btn-light"
+              >
+                Go back
+              </Link>
               <h1 className="display-4 text-center mt-5">Edit Profile</h1>
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
