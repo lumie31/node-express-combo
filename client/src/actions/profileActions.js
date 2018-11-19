55f645f2e1fb38ab373a9f78def03c50e97be1d2
Olumide <olumideokedusi@gmail.com>
2018-11-19 13:59:49 +0100

    Added addExperience action to profileActions file

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -41,6 +41,20 @@ export const createProfile = (profileData, history) => dispatch => {
       });
     });
 };
+//Add Experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => {
+      history.push("/dashboard");
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
 
 // Delete Account and profile
 export const deleteAccount = () => dispatch => {
