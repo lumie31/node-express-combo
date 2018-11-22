3821591bf7855e161d38beb773a58c3bb44fe2b5
Olumide <olumideokedusi@gmail.com>
2018-11-22 07:03:29 +0100

    Replace class with classname in imported template

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
@@ -14,15 +15,21 @@ class ProfileAbout extends Component {
       </div>
     ));
     return (
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card card-body bg-light mb-3">
-            <h3 class="text-center text-info">{firstName}'s Bio</h3>
-            <p class="lead">{profile.bio}</p>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{firstName}'s Bio</h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} does not have a bio</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
             <hr />
-            <h3 class="text-center text-info">Skill Set</h3>
-            <div class="row">
-              <div class="d-flex flex-wrap justify-content-center align-items-center">
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
                 {skills}
               </div>
             </div>
