9a54d240901a1ac44ae2c8187ba0a40f106791bb
Olumide <olumideokedusi@gmail.com>
2018-12-17 06:15:42 +0100

    Add component to display and comment on single post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import PostItem from "../posts/PostItem";
 import Spinner from "../common/Spinner";
 import { getPost } from "../../actions/postActions";
 
@@ -10,9 +12,31 @@ class Post extends Component {
   }
 
   render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <h1 style={{ marginTop: "10vh" }}>Single Post here</h1>
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div style={{ marginTop: "10vh" }} className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back to Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
