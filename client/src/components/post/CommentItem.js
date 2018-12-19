7bf1fb5bbca0166d1fbecdc2b307850ff645d6c3
Olumide <olumideokedusi@gmail.com>
2018-12-19 16:16:54 +0100

    Add comment item component

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -6,7 +6,26 @@ import { deleteComment } from "../../actions/postActions";
 class CommentItem extends Component {
   render() {
     const { comment, postId, auth } = this.props;
-    return <div />;
+    return (
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
+            <a href="profile.html">
+              <img
+                className="rounded-circle d-none d-md-block"
+                src={comment.avatar}
+                alt=""
+              />
+            </a>
+            <br />
+            <p className="text-center">{comment.name}</p>
+          </div>
+          <div className="col-md-10">
+            <p className="lead">{comment.text}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
